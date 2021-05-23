3969687ecaab35b749048db4002a5d9fbe8f0f18
Mark Stouffer <mark@thefullertonian.com>
2021-05-23 16:57:30 -0700

    updates

--- content/_index.md
+++ content/_index.md
@@ -1,5 +1,5 @@
 ---
-title: Graph Writer
+title: My Web Content
 date: 2021-05-23
 author: Mark Stouffer
 ---
---
+++ deploy.ps1
@@ -0,0 +1,4 @@
+hugo;
+Set-Content -Path 'docs/CNAME' -Value 'http://mstouffer.com'
+git add .; git commit -m "updates";git push;
+
